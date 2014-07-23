c33b3cf9b0637b4ff453df78c252e70554df0750
26746647 <sai.sai@163.com>
2014-07-23 16:41:31 +0800

    增加数据填充的样例

--- example/src/za/co/immedia/pinnedheaderlistviewexample/TestSectionedAdapter.java
+++ example/src/za/co/immedia/pinnedheaderlistviewexample/TestSectionedAdapter.java
@@ -1,6 +1,11 @@
 package za.co.immedia.pinnedheaderlistviewexample;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -9,27 +14,46 @@ import android.widget.TextView;
 import za.co.immedia.pinnedheaderlistview.SectionedBaseAdapter;
 
 public class TestSectionedAdapter extends SectionedBaseAdapter {
+	
+	
+	
+	private ArrayList stringtitle = new ArrayList<String>();
+	
+	private ArrayList stringitem = new ArrayList<String>();
+	
+	private HashMap titleitemcount = new HashMap<String, Integer>();
+	
+	private HashMap titleitemposition = new HashMap<String, Integer>();
 
     @Override
     public Object getItem(int section, int position) {
         // TODO Auto-generated method stub
-        return null;
+    	
+    	String titlestring= stringtitle.get(section).toString();
+    	int count = Integer.parseInt(titleitemcount.get(titlestring).toString());
+        return stringitem.get(count+section);
     }
 
     @Override
     public long getItemId(int section, int position) {
         // TODO Auto-generated method stub
-        return 0;
+    	String titlestring= stringtitle.get(section).toString();
+    	int count = Integer.parseInt(titleitemcount.get(titlestring).toString());
+        return count+section;
     }
 
     @Override
     public int getSectionCount() {
-        return 7;
+        return stringtitle.size();
     }
 
     @Override
     public int getCountForSection(int section) {
-        return 15;
+    	
+    	String titlestring= stringtitle.get(section).toString();
+    	Log.d("TestSectionedAdapter ", "getCountForSection =" +titlestring);
+    	int count = Integer.parseInt(titleitemcount.get(titlestring).toString());
+        return count;
     }
 
     @Override
@@ -41,7 +65,15 @@ public class TestSectionedAdapter extends SectionedBaseAdapter {
         } else {
             layout = (LinearLayout) convertView;
         }
-        ((TextView) layout.findViewById(R.id.textItem)).setText("Section " + section + " Item " + position);
+        
+        String titlestring= stringtitle.get(section).toString();
+
+        int count = Integer.parseInt(titleitemposition.get(titlestring).toString());
+        Log.d( "TestSectionedAdapter ", "title =" +titlestring + "start= "+titleitemposition.get(titlestring).toString()+"p="+(count+position));
+        
+        String itemstring=  stringitem.get(count+position).toString();
+       
+        ((TextView) layout.findViewById(R.id.textItem)).setText(" " + titlestring + " Item =" + itemstring);
         return layout;
     }
 
@@ -54,8 +86,47 @@ public class TestSectionedAdapter extends SectionedBaseAdapter {
         } else {
             layout = (LinearLayout) convertView;
         }
-        ((TextView) layout.findViewById(R.id.textItem)).setText("Header for section " + section);
+        String titlestring= stringtitle.get(section).toString();
+        ((TextView) layout.findViewById(R.id.textItem)).setText("Header for section " + titlestring);
         return layout;
     }
 
+	public ArrayList getStringtitle() {
+		return stringtitle;
+	}
+
+	public void setStringtitle(ArrayList stringtitle) {
+		this.stringtitle = stringtitle;
+	}
+
+	public ArrayList getStringitem() {
+		return stringitem;
+	}
+
+	public void setStringitem(ArrayList stringitem) {
+		this.stringitem = stringitem;
+	}
+
+	public HashMap getTitleitemcount() {
+		return titleitemcount;
+	}
+
+	public void setTitleitemcount(HashMap titleitemcount) {
+		this.titleitemcount = titleitemcount;
+	}
+
+	public HashMap getTitleitemposition() {
+		return titleitemposition;
+	}
+
+	public void setTitleitemposition(HashMap titleitemposition) {
+		this.titleitemposition = titleitemposition;
+	}
+
+
+
+    
+    
+    
+    
 }
